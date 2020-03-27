a0e3f30b55de4f27c1c89c5cf5eb203261f810eb
Mishra <archana.mishra@accenture.com>
2020-03-27 11:33:59 +0530

    Read CSV data into Shipment using csvhelper

--- ShipmentValidation.Api/Controllers/ShipmentValidationController.cs
+++ ShipmentValidation.Api/Controllers/ShipmentValidationController.cs
@@ -1,11 +1,49 @@
+using CsvHelper;
 using Microsoft.AspNetCore.Mvc;
+using ShipmentValidation.Api.Models;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 
 namespace ShipmentValidation.Api.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class ShipmentValidationController :ControllerBase
+    public class ShipmentValidationController : ControllerBase
     {
-        
+        private  List<Shipment> _shipments;
+        //public ShipmentValidationController(List<Shipment> shipments)
+        //{
+         //   _shipments = shipments;
+
+        //}
+        /// Load shipments from file
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        private void LoadShipments()
+        {
+            using (var reader = new StreamReader("C:\\Users\\archana.mishra\\Documents\\SampleData\\Shipment_NoError.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                _shipments = csv.GetRecords<Shipment>().ToList();
+            }
+        }
+
+        [HttpGet]
+        public IEnumerable<Shipment> Get()
+        {
+            using (var reader = new StreamReader("C:\\Users\\archana.mishra\\Documents\\SampleData\\Shipment_NoError.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                _shipments = csv.GetRecords<Shipment>().ToList();                
+            }
+            return _shipments; 
+
+        }
+
+
+
     }
 }
--- ShipmentValidation.Api/Models/Shipment.cs
+++ ShipmentValidation.Api/Models/Shipment.cs
@@ -1,15 +1,22 @@
 using System;
+using CsvHelper.Configuration.Attributes;
 
 namespace ShipmentValidation.Api.Models
 {
     
     public class Shipment
     {
+        [Name("Shipment Origin")]
         public Location ShipmentOrigin { get; set; }
+        [Name("Shipment Destination")]
         public Location ShipmentDestination { get; set; }
+        [Name("Pickup Date")]
         public DateTime PickUpDate { get; set; }
+        [Name("Weight")]
         public decimal Weight { get; set; }
+        [Name("Cube")]
         public decimal Cube { get; set; }
+        [Name("Service Type")]
         public ServiceType serviceType { get; set; }
     }
 
