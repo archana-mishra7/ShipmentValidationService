78b1cff8b647d7ab98a1b87bfa74efcfbc9a6c11
Mishra <archana.mishra@accenture.com>
2020-03-27 19:44:00 +0530

    added CSV error check for Headers by csvHelper

--- ShipmentValidation.Api/Controllers/ShipmentValidationController.cs
+++ ShipmentValidation.Api/Controllers/ShipmentValidationController.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using ShipmentValidation.Api.Dtos;
+using System.Threading.Tasks;
 
 namespace ShipmentValidation.Api.Controllers
 {
@@ -13,6 +15,7 @@ namespace ShipmentValidation.Api.Controllers
     public class ShipmentValidationController : ControllerBase
     {
         private  List<Shipment> _shipments;
+        private List<ShipmentErrorLog>  _errorLogs = new List<ShipmentErrorLog>();
         //public ShipmentValidationController(List<Shipment> shipments)
         //{
          //   _shipments = shipments;
@@ -24,26 +27,60 @@ namespace ShipmentValidation.Api.Controllers
         /// <returns></returns>
         private void LoadShipments()
         {
-            using (var reader = new StreamReader("C:\\Users\\archana.mishra\\Documents\\SampleData\\Shipment_NoError.csv"))
-            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            try
             {
-                _shipments = csv.GetRecords<Shipment>().ToList();
+                bool isRecordBad = false;
+                using (var reader = new StreamReader("C:\\Users\\archana.mishra\\Documents\\SampleData\\ShipmentFile.csv"))
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    
+                    csv.Configuration.BadDataFound = context =>
+                        {
+                            isRecordBad = true;
+
+                            var errorLog = new ShipmentErrorLog();
+                            errorLog.ErrorType = "File Error";
+                            errorLog.ErrorValue = context.RawRecord;
+                            _errorLogs.Add(errorLog);
+                        };
+                    csv.Configuration.HeaderValidated = (isValid, headerNames, headerNameIndex, context) =>
+                    {
+                        foreach (var header in headerNames)
+                        {
+                            if (!isValid)
+                            {
+                                isRecordBad = true;
+                                var errorLog = new ShipmentErrorLog();
+                                errorLog.ErrorType = "File header Error in "  + header;
+                                errorLog.ErrorValue =  header + " is either missing or wrong entry";
+                                _errorLogs.Add(errorLog);
+                            }
+                        }
+
+                    } ;
+                   _shipments= csv.GetRecords<Shipment>().ToList();                   
+
+                }
             }
+            catch(CsvHelperException ex)
+            {
+                var errorLog = new ShipmentErrorLog();
+                errorLog.ErrorType = "Shipment load Error";
+                errorLog.ErrorValue = ex.Message;
+                _errorLogs.Add(errorLog);
+            }
+            
         }
 
         [HttpGet]
-        public IEnumerable<Shipment> Get()
+        public IActionResult GetAllShipments(bool includeError = false)
         {
-            using (var reader = new StreamReader("C:\\Users\\archana.mishra\\Documents\\SampleData\\Shipment_NoError.csv"))
-            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
-            {
-                _shipments = csv.GetRecords<Shipment>().ToList();                
-            }
-            return _shipments; 
-
-        }
-
-
-
+            LoadShipments();
+            if(_errorLogs.Count>0)
+            return Ok(_errorLogs);
+            
+            return Ok(_shipments);
+        }  
+        
     }
 }
---
+++ ShipmentValidation.Api/Dtos/ShipmentErrorLog.cs
@@ -0,0 +1,8 @@
+namespace ShipmentValidation.Api.Dtos
+{
+    public class ShipmentErrorLog
+    {
+        public string ErrorType { get; set; }
+        public string ErrorValue { get; set; }
+    }
+}
